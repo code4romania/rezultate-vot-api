3324922a0d110ca533aa9fbb2f93157928497c19
Bogdan Bujdea <bogdan@thewindev.net>
2020-09-17 13:02:13 +0300

    Added API project with docker-compose

---
+++ src/ElectionResults.API/Controllers/BallotsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ElectionResults.API.Controllers
+{
+    [ApiController]
+    [Route("api/ballots")]
+    public class BallotsController : ControllerBase
+    {
+        private readonly ILogger<BallotsController> _logger;
+
+        public BallotsController(ILogger<BallotsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello";
+        }
+    }
+}
