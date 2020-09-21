f8a942153b5bf26e7bbb5e89775170cca71967dc
Bogdan Bujdea <bogdan@thewindev.net>
2020-09-21 11:31:42 +0300

    Changed URLs for http and https

--- src/ElectionResults.API/Program.cs
+++ src/ElectionResults.API/Program.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace ElectionResults.API
 {
@@ -21,6 +15,7 @@ namespace ElectionResults.API
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://*80;https://*:443");
                 });
     }
 }
