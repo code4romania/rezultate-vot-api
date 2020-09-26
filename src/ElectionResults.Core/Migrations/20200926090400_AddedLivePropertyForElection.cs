3a8b0b5607f776bdc7549418b5cfb1e5c79192fc
Bogdan Bujdea <bogdan@thewindev.net>
2020-09-26 12:12:06 +0300

    Added migrations

---
+++ src/ElectionResults.Core/Migrations/20200926090400_AddedLivePropertyForElection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ElectionResults.Core.Migrations
+{
+    public partial class AddedLivePropertyForElection : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Live",
+                table: "elections",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Live",
+                table: "elections");
+        }
+    }
+}
