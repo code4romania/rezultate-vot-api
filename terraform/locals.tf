ae4776768a808089cbc73cb1ff852a7a763bc15d
Andrei Ioniță <hi@andrei.io>
2024-06-06 13:41:11 +0100

    wip

---
+++ terraform/locals.tf
@@ -0,0 +1,39 @@
+locals {
+  namespace         = "rezultatevot-${var.env}"
+  availability_zone = data.aws_availability_zones.current.names[0]
+
+  images = {
+    api = {
+      image = "code4romania/rezultate-vot-api"
+      tag   = "1.1.0-rc.1"
+    }
+
+  }
+
+  ecs = {
+    instance_types = {
+      "t3a.medium" = ""
+    }
+  }
+
+  db = {
+    name           = "rezultatevot"
+    instance_class = var.env == "production" ? "db.t4g.small" : "db.t4g.micro"
+  }
+
+  networking = {
+    cidr_block = "10.0.0.0/16"
+
+    public_subnets = [
+      "10.0.1.0/24",
+      "10.0.2.0/24",
+      "10.0.3.0/24"
+    ]
+
+    private_subnets = [
+      "10.0.4.0/24",
+      "10.0.5.0/24",
+      "10.0.6.0/24"
+    ]
+  }
+}
