f70c3a6f72bfcf3f227d5c4ddf03164fe3bc80d6
Lupu Gheorghe <gheorghelupu17@gmail.com>
2024-06-09 05:51:03 +0300

    bump version gcm

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -5,12 +5,12 @@ locals {
   images = {
     api = {
       image = "code4romania/rezultate-vot-api"
-      tag   = "1.1.0-rc.4"
+      tag   = "1.1.0-rc.5"
     }
 
     hangfire = {
       image = "code4romania/rezultate-vot-hangfire"
-      tag   = "1.1.0-rc.4"
+      tag   = "1.1.0-rc.5"
     }
   }
 
